a28dea33f1f3e5a0584776a2e1abbd2d662712ad
Dimas Febriyanto <dimassfeb@gmail.com>
2025-01-15 21:20:42 +0700

    fix: position add and delete btn image menu

--- src/pages/menu/components/CardMenu.tsx
+++ src/pages/menu/components/CardMenu.tsx
@@ -29,7 +29,7 @@ export default function CardMenu({
       >
         {/* Overlay for Out of Stock */}
         {!product.is_available && (
-          <div className="absolute top-0 left-0 z-30 flex items-center justify-center w-full h-full bg-gray-800 bg-opacity-50 rounded-md">
+          <div className="absolute top-0 left-0 z-20 flex items-center justify-center w-full h-full bg-gray-800 bg-opacity-50 rounded-md">
             <div className="text-lg font-bold text-white">Out of Stock</div>
           </div>
         )}
@@ -75,55 +75,55 @@ export default function CardMenu({
       {/* Image Click Overlay */}
       {onImageClick && (
         <div
-          className="fixed inset-0 z-10 flex items-center justify-center p-4 bg-gray-900 bg-opacity-50"
+          className="fixed inset-0 z-30 flex items-center justify-center p-4 bg-gray-900 bg-opacity-50"
           onClick={() => setOnImageClick(false)}
         >
-          <div className="w-[28rem] h-[40em] bg-secondary">
+          <div className="relative w-[28rem] h-[40em] bg-secondary rounded-lg shadow-lg overflow-hidden">
             {/* Content */}
             <img
               src={product.image_url}
               alt={product.name}
-              className="w-full"
+              className="w-full h-2/5 object-cover"
             />
 
             {/* Product Details */}
-            <div className="p-5">
+            <div className="p-5 h-3/5 flex flex-col justify-between">
               {/* Name and Rating */}
-              <div className="flex items-center justify-between mb-3">
-                <div className="font-bold">{product.name}</div>
-                <div className="flex items-center gap-1 px-2 py-1 text-sm text-white bg-black border rounded-lg">
-                  <span>{product.rating}</span>
-                  <Star fontSize="small" className="text-yellow-500" />
+              <div>
+                <div className="flex items-center justify-between mb-3">
+                  <div className="font-bold text-lg">{product.name}</div>
+                  <div className="flex items-center gap-1 px-2 py-1 text-sm text-white bg-black border rounded-lg">
+                    <span>{product.rating}</span>
+                    <Star fontSize="small" className="text-yellow-500" />
+                  </div>
                 </div>
-              </div>
 
-              {/* Description */}
-              <div className="mb-3 text-gray-700">{product.description}</div>
+                {/* Description */}
+                <div className="mb-3 text-gray-700">{product.description}</div>
 
-              {/* Price */}
-              <div className="mb-5 text-lg font-semibold">
-                {formatToRupiah(product.price)}
+                {/* Price */}
+                <div className="text-lg font-semibold">
+                  {formatToRupiah(product.price)}
+                </div>
               </div>
 
               {/* Action Buttons */}
-              <div className="relative right-0 bottom-0">
-                <div className="flex gap-2">
-                  <button
-                    onClick={() => setOnImageClick(false)}
-                    className="px-4 py-2 text-black bg-white border border-gray-300 rounded-md hover:bg-gray-100"
-                  >
-                    Batal
-                  </button>
-                  <button
-                    onClick={() => {
-                      onAdd(product);
-                      setOnImageClick(false);
-                    }}
-                    className="px-4 py-2 text-white bg-black rounded-md hover:bg-gray-800"
-                  >
-                    Tambah
-                  </button>
-                </div>
+              <div className="flex justify-end gap-2 mt-5">
+                <button
+                  onClick={() => setOnImageClick(false)}
+                  className="px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-100"
+                >
+                  Batal
+                </button>
+                <button
+                  onClick={() => {
+                    onAdd(product);
+                    setOnImageClick(false);
+                  }}
+                  className="px-4 py-2 text-white bg-black rounded-md hover:bg-gray-800"
+                >
+                  Tambah
+                </button>
               </div>
             </div>
           </div>
